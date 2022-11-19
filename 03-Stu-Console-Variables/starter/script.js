16f1425161def99b94718c775d492fbcb411a69b
Mercy <116111819+MercyAz@users.noreply.github.com>
2022-11-19 21:55:05 +0000

    solved activity

--- 03-Stu-Console-Variables/starter/script.js
+++ 03-Stu-Console-Variables/starter/script.js
@@ -1,1 +1,13 @@
 // Write Your JavaScript Code Here
+
+var personName = ('Mercy.')
+
+console.log ('My name is ' + personName )
+
+var petName = ['Chubakka' , 'Rico']
+
+console.log ('The name of my Pet is ' + petName [0])
+
+var funFact = ('I love Chickens!')
+
+console.log ('Funfact: '+ funFact)
